f5465ac8e3ca88418f204059486125925be8312f
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2025-09-30 12:00:38 +0200

    fix: Update site title and description in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Synthwave";
+export const SITE_DESCRIPTION =
+  "AI-Powered Insights & Creative Tech Adventures by Siyabonga Lukhele";
